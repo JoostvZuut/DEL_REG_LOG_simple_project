5e9d72e8fca138a9bc6f24d1b738829304fb87ed
JoostvZuut <95977559+JoostvZuut@users.noreply.github.com>
2021-12-18 17:05:17 +0100

    Update Joos_Farah_0889548_ITSMVT1A.py

--- Joos_Farah_0889548_ITSMVT1A.py
+++ Joos_Farah_0889548_ITSMVT1A.py
@@ -6,25 +6,30 @@ from pathlib import Path
 username= ''
 password= ''
 
+if easygui.ccbox(msg="Welkom op onze platform", title="Startscherm"):
+     pass
+else:
+     quit()
+
 def mainmenu():
     msg = ("Selecteer uw keuze")
-    title = "Opties menu"
+    title = "Hoofdmenu"
     choices = [ "Inloggen", "Registreren", "Gebruiker verwijderen"]
     choice = easygui.choicebox(msg, title, choices)
     print(choice)
     if choice == "Inloggen":
-        loginscreen()
+        loginscherm()
     elif choice == "Registreren":
-        registratiescreen()
+        registratiescherm()
     elif choice == "Gebruiker verwijderen":
-        deleteuser()
+        verwijdergebruikerscherm()
 
-def registratiescreen():
+def registratiescherm():
     msg= "Registreer"
     title= "Registratieformulier"
     fieldNames= ['Gebruikersnaam', 'Wachtwoord']
     fieldValues= []
-    fieldValues= multpasswordbox(msg, title, fieldNames)
+    fieldValues= easygui.multpasswordbox(msg, title, fieldNames)
     if fieldValues == None:
         mainmenu()
 
@@ -32,37 +37,37 @@ def registratiescreen():
     password= fieldValues[1]
 
     hashedpw= hashlib.sha1(password.encode("utf-8")).hexdigest()
-    f= open("userpass.txt", "a+")
+    f= open("userinfo.txt", "a+")
     f.write('\n')
     f.write(username)
     f.write(hashedpw)
-    f.close()
-    registratiescreen()
+    easygui.ccbox(msg="Bedankt voor uw registratie, u kunt nu inloggen.", title= "Registratie")
+    mainmenu()
 
 
-def loginscreen():
-    msg= "Login met uw gebruikersnaam en wachtwoord"
+def loginscherm():
+    msg= "U kunt inloggen met uw gebruikersnaam en wachtwoord"
     title= "Loginscherm"
-    fieldNames= ['Gebrukersnaam', 'Wachtwoord']
+    fieldNames= ['Gebruikersnaam', 'Wachtwoord']
     fieldValues= []
     fieldValues= easygui.multpasswordbox(msg, title, fieldNames)
     if fieldValues == None:
         mainmenu()
-    usernameAttempt = fieldValues[0]
+    usernameAttempt= fieldValues[0]
     passwordAttempt= fieldValues[1]
     hashedpwAttempt= hashlib.sha1(passwordAttempt.encode('utf-8')).hexdigest()
 
-    with open("userpass.txt") as myfile:
+    with open("userinfo.txt") as myfile:
         if usernameAttempt and hashedpwAttempt in myfile.read():
-            easygui.msgbox('Welkom, u bent ingelogd')
+            easygui.msgbox("Welkom u bent ingelogd.", ok_button= "Beëindigen")
             loginscreen()
         else:
             easygui.msgbox("Gebruikersnaam of wachtwoord is onjuist.")
             loginscreen()
+    mainmenu()
 
-    print(fieldValues)
 
-def deleteuser():
+def verwijdergebruikerscherm():
     msg= 'Type de gebruikersnaam van de gebruiker die u wenst te verwijderen.'
     title= "Gebruiker verwijderen"
     fieldNames= [""]
@@ -71,15 +76,17 @@ def deleteuser():
     if fieldValues == None:
         mainmenu()
 
-    myfile = open(userpass.txt, "rt")
-    data= myfile.read()
-    data= data.replace(usernametoDelete, 'null')
-    myfile.close()
-    myfile= open("userpass.txt", 'wt')
-    myfile.write(data)
-    myfile.close()
-    easygui.msgbox("Gebruiker verwijderd.")
-    mainmenu()
+    with open("userinfo.txt") as myfile:
+        if username in myfile.read():
+        myfile = open(userinfo.txt, "rt")
+        data= myfile.read()
+        data= data.replace(usernametoDelete, 'null')
+        myfile.close()
+        myfile= open("userinfo.txt", 'wt')
+        myfile.write(data)
+        myfile.close()
+        easygui.msgbox("Gebruiker is succesvol verwijderd.")
+        mainmenu()
 
 
 mainmenu()
